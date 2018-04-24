ab7adea304016aca3a3650990d9261e3561d2407
Stéphane Goudeau <stephgou@microsoft.com>
2018-04-24 16:58:44 +0200

    Unit test simplification

--- EthereumDApp/test/chainpoint.js
+++ EthereumDApp/test/chainpoint.js
@@ -1,21 +1,17 @@
 var ChainPoint = artifacts.require("./ChainPoint.sol");
 
-contract('ChainPoint', function(accounts) {
-  it("checkdate should be set after check", function() {
- 
+contract('ChainPoint', (accounts) => {
+  it('checkdate should be set after check', async () => {
+    
+    const instance = await ChainPoint.deployed()
+
     var userid = "0F623638-9B01-4ca6-A553-72709801DB1C";
     var username = "scott"
 
-    var instance = null;
-    //var checkpointdate = 0;
+    await instance.check(userid, username, 1);
 
-    return ChainPoint.deployed().then(function(deployedcontract) {
-      instance = deployedcontract;
-      return instance.check(userid, username, 1);
-    }).then(function() {
-      return instance.getCheckDate(userid);
-    }).then(function(checkpointdate) {
-        assert.notEqual(checkpointdate, 0, "value of checkpointdate should not be null");
-      })
-  }).timeout(40000); 
-});
+    const checkpointdate = await instance.getCheckDate(userid);
+
+    assert.notEqual(checkpointdate, 0, "value of checkpointdate should not be null");
+  })
+})
